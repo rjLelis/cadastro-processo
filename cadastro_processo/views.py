e69035b60b830f6199690f0736b8648b9531dc2f
rjLelis <renatojlelis@gmail.com>
2019-03-27 08:58:22 -0300

    Criação do form de planilha

--- cadastro_processo/views.py
+++ cadastro_processo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .forms import PlanilhaForm
 
-# Create your views here.
+def cadastro(request):
+    if request.method == 'POST':
+        form = PlanilhaForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            nome = form.cleaned_data.get('nome')
+            print(f'Planilha do cliente {nome} cadastrada com sucesso')
+    else:
+        form = PlanilhaForm()
+    return render(request, 'cadastro_processo/home.html', {'form': form})
